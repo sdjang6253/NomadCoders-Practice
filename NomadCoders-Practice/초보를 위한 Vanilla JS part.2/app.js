c8f639aca83c33cf016b59da080bcaa288dea6b1
sdjang6253 <qkswl625341@gmail.com>
2019-06-10 16:14:36 +0900

    노마드 코더 동영상 따라하기 완료.
    개인적으로 추가한 부분
    1) 그림을 그리다가 캔버스를 나갔다 다시 돌아오면 그림이 끊긴다-> 끊기지 않고 지속적으로 그리도록 수정
    2) 어떤 색을 선택했는지 알기가 어렵다 -> 색상을 클릭하면 클릭한 색상이 눈에띄게 수정
    3) 마우스 커서가 너무 그림그리는거 같지가 않다 -> 펜모양의 이모지를 사용하고 싶었으나, 이모지의 크기가 너무 크고 생성되는 위치를 어떻게 바꿔야 할지 모르겠어서 그냥 CrossHair 로 수정
    4) 전체 색칠을 해놓고 캔버스에서 마우스 클릭후 움직이면 선이 그려지다 전체 페인트가 이루어짐 -> 해당 부분 수정
    
    추가하고 싶은 부분
    1) 지우개를 만들자.

--- NomadCoders-Practice/초보를 위한 Vanilla JS part.2/app.js
+++ NomadCoders-Practice/초보를 위한 Vanilla JS part.2/app.js
@@ -2,29 +2,42 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
-canvas.width=700;
-canvas.height = 700;
-
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth= 2.5;
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+const body = document.getElementsByTagName("body")[0];
 
 
+const INITIAL_COLOR="#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth= 5;
+
+
+let filling = false;
 let painting = false;
 
 function stopPainting(){
     painting = false;
 }
-function startPainting(){
+function startPainting(event){
    painting = true; 
 }
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
+
+    if(filling == false && painting == false){
         ctx.beginPath();
         ctx.moveTo(x,y);
-    } else{
+    } else if (filling == false && painting == true){
         ctx.lineTo(x,y);
         ctx.stroke();
     }
@@ -37,17 +50,86 @@ function onMouseDown(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    clearBtnSelect();
+    event.target.style.border = "3px solid black";
 }
 
 function handleRangeChange(event){
     ctx.lineWidth = event.target.value;
 }
 
+function handleModeClick(event){
+    if(filling == true){
+        filling = false;
+        mode.innerText = "fill";
+    } else {
+        filling = true;
+        mode.innerText = "paint";
+    }
+}
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(event){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = 'PaintJS[🎨]';
+    link.click();
+}
+
+function handleCursorLeave(event){
+    body.style.cursor = "auto";
+    if(filling == false && painting == true){
+        const x = event.offsetX;
+        const y = event.offsetY;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+        
+    }
+}
+
+function handleCursorEnter(event){
+    //body.style.cursor = "CrossHair";
+    body.style.cursor = "url('pen_emoji.png'), auto";
+    body.style.cursor = " col-resiez:5px ; row-resize : 5px"
+    if(filling == false && painting == true){
+        let x = event.offsetX;
+        let y = event.offsetY;
+        if( x >= CANVAS_SIZE - 10 )
+            x = CANVAS_SIZE;
+        else if( x <= 10 )
+            x = 0;
+        if( y >= CANVAS_SIZE - 10 )
+            y = CANVAS_SIZE;
+        else if( y <=  10 )
+            y = 0;
+        console.log( x , y);
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
+}
+
+function clearBtnSelect(){
+    Array.from(colors).forEach(color => color.style.border = "0px")
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave",stopPainting);
+    //canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave",handleCursorLeave);
+    canvas.addEventListener("mouseenter",handleCursorEnter);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click",handleColorClick))
@@ -55,3 +137,13 @@ Array.from(colors).forEach(color => color.addEventListener("click",handleColorCl
 if(range){
     range.addEventListener("input", handleRangeChange);
 }
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
+
+body.addEventListener('mouseup', stopPainting);
