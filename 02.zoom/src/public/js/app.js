44c45e01758731ee716d3843572aa9a9742007c9
sdjang6253 <qkswl625341@gmail.com>
2023-01-08 20:15:41 +0900

    #3.2 Camera with Https Setting 나중에는 nginx추가해서 그곳으로 옮겨보기

--- 02.zoom/src/public/js/app.js
+++ 02.zoom/src/public/js/app.js
@@ -3,20 +3,53 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById('cameras');
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia(){
+async function getCameras() {
+    try {
+        const devices =await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === 'videoinput');
+        const currentCamera = myStream.getVideoTracks()[0];
+
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if(currentCamera.label === camera.label){
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option);
+        })
+        console.log(cameras);
+    }catch (e){
+        console.log(e);
+    }
+}
+
+async function getMedia(deviceId){
+    const  initialConstraints = {
+        audio : true,
+        video : {facingMode : 'user'},
+    };
+    const cameraConstraints = {
+        audio : true,
+        video : {
+            deviceId : { exact : deviceId } 
+        }
+    }
     try{
-        myStream = await navigator.mediaDevices.getUserMedia({
-            audio: true , 
-            video: true
-        });
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstraints : initialConstraints
+        );
         myFace.srcObject = myStream;
-        console.log(myStream);
-    } catch(e){
+        if(!deviceId){
+            await getCameras();
+        }
+        } catch(e){
         console.log(e);
     }
 }
@@ -24,6 +57,10 @@ async function getMedia(){
 getMedia();
 
 function handleMuteClick () {
+    console.log(myStream.getAudioTracks());
+    myStream.getAudioTracks().forEach((track) => {
+        track.enabled = !track.enabled       
+    });
     if(!muted){
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -33,6 +70,10 @@ function handleMuteClick () {
     }
 }
 function handleCameraClick () {
+    console.log(myStream.getVideoTracks());
+    myStream.getVideoTracks().forEach((track) => {
+        track.enabled = !track.enabled       
+    });
     if(cameraOff){
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
@@ -42,5 +83,11 @@ function handleCameraClick () {
     }
 }
 
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener('click' , handleMuteClick);
 cameraBtn.addEventListener('click' , handleCameraClick);
+camerasSelect.addEventListener('input' , handleCameraChange);
+
--- 02.zoom/src/server.js
+++ 02.zoom/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
 import http from "http";
+import https from 'https';
+import fs from 'fs';
 import {Server} from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 //강의에서는 __dirname 이 기본으로 가져와 지지만 나는 그게 안되어서 임의로 가져와줌.
 // package.json 에서 type:module 을 추가한 뒤로 이렇게 되는것 같음. 
 import path from "path";
+
+const options = {
+    key: fs.readFileSync('/mnt/01.Development/rootca.key'),
+    cert: fs.readFileSync('/mnt/01.Development/rootca.crt')
+  };
 
 const app = express();
 const __dirname = path.resolve();
@@ -20,8 +27,13 @@ app.get("/*" , (req, res) => res.redirect("/"));
 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const handleHttpsListen = () => console.log(`Listening on http://localhost:3003`);
 
 const httpServer = http.createServer(app);// websocker 을 하기위해 server 를 명시적? 으로 생성
+const httpsServer = https.createServer(options, app);
 const wsServer = new Server(httpServer);
+const httpsWsServer = new Server(httpsServer);
 
 httpServer.listen(3000, handleListen); // app.listen() 이랑 별반 차이 없어보이지만 이로 인해 http 와 ws 를 둘다 구동 가능
+httpsServer.listen(3003, handleHttpsListen); 
+//2023.01.08  당장은 https 를 node 에서 올리지만, nginx를 추가해서 해당 설정 nginx로 옮길수 있도록 수정 
